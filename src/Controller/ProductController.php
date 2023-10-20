8acb4ad5ab1c943e75092c6dd4bf46765f3a565f
Vojtěch Mimochodek <43761153+Repk1ns@users.noreply.github.com>
2023-10-20 10:31:28 +0200

    Factory + Drivers initialization

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,11 +5,19 @@ namespace App\Controller;
 class ProductController extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractController
 {
 
+    private \App\Factory\MySQLProductFactory $productFactory;
+
+    public function __construct(
+        \App\Factory\MySQLProductFactory $productFactory,
+    ) {
+        $this->productFactory = $productFactory;
+    }
+
 
     public function detail(int $id): \Symfony\Component\HttpFoundation\Response
     {
-        
+        $product = $this->productFactory->getProductById($id);   
 
-        return new \Symfony\Component\HttpFoundation\Response();
+        return new \Symfony\Component\HttpFoundation\Response((string)$product);
     }
 }
